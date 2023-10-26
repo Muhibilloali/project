068df334d02d93bc7ab4e3ff551bf4c35dde7421
Muxibillo <134770432+Muhibilloali@users.noreply.github.com>
2023-10-26 13:39:35 +0300

    Add files via upload

---
+++ coffee menu/coffee.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bars');
+let nav = document.querySelector('.navbar-nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    nav.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
